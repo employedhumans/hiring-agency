a38234f3d35f2724ad710dc1d99280376f4494ff
M0rfes <fahimkhan20148@gmail.com>
2019-12-02 13:33:13 +0530

    added formick

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
+import ContactForm from './components/ContactForm';
+
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>{process.env.REACT}</code> and save to reload.
-        </p>
-        <Button>Hello</Button>
-      </header>
-    </div>
+    <>
+      <ContactForm />
+    </>
   );
 };
 
